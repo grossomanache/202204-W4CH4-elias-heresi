77ddf27d98cdf2ec1c6c533eb8607c9ad260e8c1
Elías Heresi <elias.heresi@gmail.com>
2022-04-28 21:43:28 +0200

    Created Keyboard test

---
+++ src/components/Keyboard/Keyboard.test.js
@@ -0,0 +1,21 @@
+import Keyboard from "./Keyboard";
+import { render, screen } from "@testing-library/react";
+import PhoneContext from "../../contexts/PhoneContext";
+
+describe("Given the Keyboard component", () => {
+  describe("When the component is instantiated", () => {
+    test("Then 11 keys will be created", () => {
+      const addDigit = () => {};
+      const expectedNumberOfKeys = 11;
+
+      render(
+        <PhoneContext.Provider value={addDigit}>
+          <Keyboard />
+        </PhoneContext.Provider>
+      );
+
+      const createdDisplay = screen.getAllByRole("button");
+      expect(createdDisplay.length).toBe(expectedNumberOfKeys);
+    });
+  });
+});
